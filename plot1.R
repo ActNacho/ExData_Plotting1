92766d7cb03a442d6a095de7b9e646be8845a61a
IGNACIO TRUJILLO <trujillo_itm@hotmail.com>
2015-02-08 15:05:58 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,29 @@
+## Before start, set the working directory to the place where all R scripts and
+## the datafile (household_power_consumption.txt) are. All the scripts and the
+## txt must be in the same path.
+
+## Load the large dataset.
+data <- read.table("household_power_consumption.txt", 
+                   header = TRUE, sep = ";", 
+                   colClasses = c("character", "character", 
+                   rep("numeric",7)), na = "?")
+## Keep only the relevant dates
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
+## Add a proper format to the time variable
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+## Rename the rows of the data to match the "real" value in the new dataset
+rownames(data) <- 1:nrow(data)
+
+## Make the plot 1
+png(filename = "plot1B.png", 
+    width = 480, 
+    height = 480, 
+    units = "px", 
+    bg = "transparent")
+hist(newData$Global_active_power, 
+     col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)",
+     breaks = 12, 
+     ylim = c(0, 1200))
+dev.off()
